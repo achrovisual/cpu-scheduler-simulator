9cc40a3568e401af05e062f808f6feb0abee6e76
RafMdrzo <rafael_maderazo@dlsu.edu.ph>
2021-01-15 23:10:21 +0800

    file created, thought process placed

---
+++ filereading.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_STRING 5000
+#define MAX 100
+
+int main(){
+    char string[MAX_STRING];
+    int params[3];
+    int processValues[MAX][3];
+
+    //read through file
+
+    //filter \n,\(space), and \r from string array, store first 3 vals in params, the rest in processValues
+    //NOTE: after populating processValues, check the final value of the row Iterator if it is == to params[1] (use while loop)
+
+    //check parameters from param array
+
+    
+    return 0;
+}
