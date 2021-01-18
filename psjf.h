c1787f871a6fe70b0389ee410bd6f724d6268573
Eugenio Pastoral <stonenobrein@gmail.com>
2021-01-18 13:50:58 +0800

    Cleaned up code

--- psjf.h
+++ psjf.h
@@ -1,6 +1,28 @@
 #define MAX_VAL 999
 
-void psjf_sort_process(int process[][6], int paused_states[][6], int n) {
+void psjf_initialize_array(int arr[][4], int size) {
+  for(int i = 0; i < size; i ++)
+  for(int j = 0; j < 4; i ++)
+  arr[i][j] = 0;
+}
+
+void psjf_clean_paused_states(int process[][6], int paused_states[][4], int *iterator, int n) {
+  //remove non-unique paused start time
+  for(int i = 0; i < *iterator; i++)
+  for(int j = 0; j < n; j++)
+  if(paused_states[i][1] == process[j][3]){
+    //shift
+    for(int k = j; k < *iterator; k++){
+      paused_states[k][0] = paused_states[k + 1][0];
+      paused_states[k][1] = paused_states[k + 1][1];
+      paused_states[k][2] = paused_states[k + 1][2];
+      paused_states[k][3] = paused_states[k + 1][3];
+    }
+    *iterator = *iterator - 1;
+  }
+}
+
+void psjf_sort_process(int process[][6], int paused_states[][4], int n) {
   for(int i = 0; i < n; i++)
   for(int j = 0; j < n - i - 1; j++)
   if(process[j][5] > process[j + 1][5]) {
@@ -12,10 +34,8 @@ void psjf_sort_process(int process[][6], int paused_states[][6], int n) {
   }
 }
 
-void psjf_calculate_waiting_time(int process[][6], int n, int paused_states[][6], int *iterator)
+void psjf_calculate_waiting_time(int process[][6], int paused_states[][4], int *iterator, int n)
 {
-  int n_process = n;
-
   int remaining_time[101], first_pass[101];
   for(int i = 0; i < n; i++){
     first_pass[i] = 1;
@@ -28,15 +48,15 @@ void psjf_calculate_waiting_time(int process[][6], int n, int paused_states[][6]
   int completed_process = 0, time = 0, smallest_burst_index = 101, min = MAX_VAL;
   int check = 0;
   *iterator = 0;
-  while(completed_process != n_process){
+  while(completed_process != n){
 
-    for(int j = 0; j < n_process; j++){
+    for(int j = 0; j < n; j++){
       if((process[j][1] <= time) && (remaining_time[j]  < min) && remaining_time[j] > 0){
         if(first_pass[j] == 1){
           paused_states[(*iterator)][0] = process[smallest_burst_index][0];
-          paused_states[(*iterator)][3] = process[smallest_burst_index][3];
-          paused_states[(*iterator)][4] = time;
-          paused_states[(*iterator)][5] = min;
+          paused_states[(*iterator)][1] = process[smallest_burst_index][3];
+          paused_states[(*iterator)][2] = time;
+          paused_states[(*iterator)][3] = min;
           (*iterator)++;
         }
 
@@ -80,76 +100,32 @@ void psjf_calculate_waiting_time(int process[][6], int n, int paused_states[][6]
 }
 
 void psjf(int process[][6], int n){
-  int time = 0, completed_process = 0, n_process = n;
   int total_waiting_time = 0;
-  int smallest_burst_index = 101;
-  int paused_states[100][6];
-  process[100][2] = MAX_VAL;
+  int paused_states[n][4];
   int iterator = 0;
 
-  psjf_calculate_waiting_time(process, n, paused_states, &iterator);
-
-  //index 0 = PID; index 1 = arrival; index 2 = burst; index 3 = start; index 4 = end; index 5 = wait
-  for(time = 0; completed_process != n_process; time++){
-    smallest_burst_index = 100;
-    for(int i = 0; i < n_process; i++){
-      if(process[i][1] <= time && process[i][2] < process[smallest_burst_index][2] && process[i][2] > 0){
-        smallest_burst_index = i;
-      }
-    }
-
-    process[smallest_burst_index][2]--;
-
-    if(process[smallest_burst_index][2] == 0){
-      completed_process++;
-    }
-  }
-
-  printf("Preemptive Shortest Job First\n");
-  //remove non-unique paused start time
-  for(int i = 0; i < iterator; i++){
-    for(int j = 0; j < n; j++){
-      if(paused_states[i][3] == process[j][3]){
-        //shift
-        for(int k = j; k < iterator; k++){
-          paused_states[k][0] = paused_states[k + 1][0];
-          paused_states[k][1] = paused_states[k + 1][1];
-          paused_states[k][2] = paused_states[k + 1][2];
-          paused_states[k][3] = paused_states[k + 1][3];
-          paused_states[k][4] = paused_states[k + 1][4];
-          paused_states[k][5] = paused_states[k + 1][5];
-        }
-        iterator--;
-      }
-    }
-  }
-
+  psjf_calculate_waiting_time(process, paused_states, &iterator, n);
+  psjf_clean_paused_states(process, paused_states, &iterator, n);
   psjf_sort_process(process, paused_states, n);
 
-  // for(int  i = 0; i < n; i++) {
-  //   // total_waiting_time = total_waiting_time + process[i][5];
-  //   printf("[%d] Start time: %d End time: %d | Waiting time: %d\n", process[i][0], process[i][3], process[i][4], process[i][5]);
-  // }
-
+  printf("Preemptive Shortest Job First\n");
   for(int  i = 0; i < n; i++) {
     total_waiting_time = total_waiting_time + process[i][5];
     int is_valid = 1;
     for(int j = 0; j < iterator; j++){
-      if(process[i][0] == paused_states[j][0] && process[i][3] != paused_states[j][3]){
-        if(is_valid == 1){
-          printf("[%d] Start time: %d End time: %d | ", paused_states[j][0], paused_states[j][3], paused_states[j][4]);
-        } else {
-          printf("Start time: %d End time: %d | ",  paused_states[j][3], paused_states[j][4]);
-        }
+      if(process[i][0] == paused_states[j][0] && process[i][3] != paused_states[j][1]){
+        if(is_valid == 1)
+        printf("[%d] Start time: %d End time: %d | ", paused_states[j][0], paused_states[j][1], paused_states[j][2]);
+        else
+        printf("Start time: %d End time: %d | ",  paused_states[j][1], paused_states[j][2]);
+
         is_valid = 0;
       }
     }
-    if(is_valid == 1){
-      printf("[%d] Start time: %d End time: %d | Waiting time: %d\n", process[i][0], process[i][3], process[i][4], process[i][5]);
-    } else {
-      printf("Start time: %d End time: %d | Waiting time: %d\n",  process[i][3], process[i][4], process[i][5]);
-    }
-
+    if(is_valid == 1)
+    printf("[%d] Start time: %d End time: %d | Waiting time: %d\n", process[i][0], process[i][3], process[i][4], process[i][5]);
+    else
+    printf("Start time: %d End time: %d | Waiting time: %d\n",  process[i][3], process[i][4], process[i][5]);
   }
 
   printf("Average waiting time: %.1f", (float)total_waiting_time / (float)n);
